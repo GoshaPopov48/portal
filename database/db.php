e5b8c944944033f941de3a363d23f1b2c71eed52
Popov Gosha <popov.goshan@mail.ru>
2024-07-24 15:42:40 +0300

    Initial project setup

---
+++ database/db.php
@@ -0,0 +1,8 @@
+<?php
+try {
+    $config = require_once __DIR__ . '/../config/database.php';
+    $db = new PDO("{$config['driver']}:host={$config['host']};dbname ={$config['dbname']};port={$config['port']}", $config['user'], $config['password']);
+} catch (PDOexception $exception) {
+    require_once __DIR__ . "/../components/db-connect-error.php";
+    die();
+}
